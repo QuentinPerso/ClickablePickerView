a616b834d7cc19f7dd3ae9384436ad36cd95d9b7
Quentin Beaudouin <quentin@marksapp.io>
2017-07-03 00:12:02 +0200

    1rst commit !

--- ClickablePickerViewExemple/ViewController.swift
+++ ClickablePickerViewExemple/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var clickablePickerView: ClickablePickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        clickablePickerView.delegate = self
+        clickablePickerView.dataSource = self
+        clickablePickerView.didSelectAction = { [weak self] in
+            print("did select row ", self?.clickablePickerView.selectedRow(inComponent: 0) ?? "no self? ?")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +29,57 @@ class ViewController: UIViewController {
 
 
 }
+
+//************************************
+// MARK: - picker view D/D
+//************************************
+
+extension ViewController:UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 8
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        
+        
+        let label = view as? UILabel == nil ? UILabel() : view as! UILabel
+        /** Uncomment to change picker font
+        label.font = UIFont(name: "Chalk", size: 15)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        */
+        if row == 0 {
+            label.text = "tomorow"
+        }
+        else {
+            let formater = DateFormatter()
+            formater.dateFormat = "MM-dd"
+            let date = Date().addingTimeInterval(Double(row+1) * 24 * 3600)
+            let dateString = DateFormatter.localizedString(from: date, dateStyle: .medium, timeStyle: .none)
+            
+            label.text = dateString
+        }
+        
+        label.textAlignment = .center
+        
+        return label
+        
+    }
+ 
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 36
+    }
+    
+    
+    
+    
+    
+}
+
 
